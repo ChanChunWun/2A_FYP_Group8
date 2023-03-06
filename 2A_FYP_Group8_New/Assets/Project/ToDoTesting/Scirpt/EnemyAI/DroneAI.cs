519f281c5a5ae743fb0a6ad8d919ac6c8d2dd4ca
CheungChoHin <210153146@stu.vtc.edu.hk>
2023-03-06 16:13:50 +0800

    DroneAi start

---
+++ 2A_FYP_Group8_New/Assets/Project/ToDoTesting/Scirpt/EnemyAI/DroneAI.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneAI : MonoBehaviour
+{
+    NavMeshAgent nav;
+    Animator anim;
+    Transform target;
+    LifeSystem lfSys;
+    float minFlyHeight = 1.5f;
+    float fixedFlyHeight;
+    RaycastHit hit;
+
+    public Transform hand;
+    public GameObject weapon;
+    public Transform shootPoint;
+
+    public float speed;
+    [Range(1.5f, 4f)]
+    public float flyHeightRange;
+    public float canShootRange = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        lfSys = GetComponent<LifeSystem>();
+        fixedFlyHeight = Random.Range(minFlyHeight, flyHeightRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lfSys.dead)
+        {
+            if (target != null)
+            {
+
+                if (Physics.Raycast(shootPoint.position, target.transform.position, out hit, canShootRange))
+                {
+                    if (hit.transform.gameObject == target)
+                    {
+                        weapon.GetComponent<TurretWeaponSystem>().Shoot(gameObject, shootPoint, null);
+                    }
+                }
+            }
+        }
+        
+    }
+}
