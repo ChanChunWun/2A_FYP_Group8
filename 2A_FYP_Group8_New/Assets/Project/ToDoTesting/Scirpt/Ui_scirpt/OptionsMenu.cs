13fc5f36b5301f0bab5c1cb997ea0559e15c5ecb
ChanChunWun <101243198+ChanChunWun@users.noreply.github.com>
2023-03-06 16:12:10 +0800

    Full Screen, Resolution Dropdown and Update

--- 2A_FYP_Group8_New/Assets/Project/ToDoTesting/Scirpt/Ui_scirpt/OptionsMenu.cs
+++ 2A_FYP_Group8_New/Assets/Project/ToDoTesting/Scirpt/Ui_scirpt/OptionsMenu.cs
@@ -2,11 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    
+
+    public Dropdown resolutionDropDown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width==Screen.currentResolution.width&&
+                resolutions[i].height==Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -15,5 +53,10 @@ public class OptionsMenu : MonoBehaviour
     public void SetQuality(int qualituIndex)
     {
         QualitySettings.SetQualityLevel(qualituIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
